40d185046013426c368bbb535802e4edd5837080
ckost9n <ckost9n@gmail.com>
2022-06-29 22:37:37 +0300

    First setup

--- VkLogin/ViewController.swift
+++ VkLogin/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
+    
+    private let myView: UIView = {
+        let element = UIView()
+        element.backgroundColor = .orange
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        view.addSubview(myView)
+        
+        setConstraints()
     }
 
 
 }
 
+
+extension ViewController {
+    private func setConstraints() {
+        myView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.width.equalTo(200)
+        }
+    }
+}
