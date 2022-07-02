56b352dc5216e7f526f39a2bda99b811208e275e
ckost9n <ckost9n@gmail.com>
2022-07-02 22:58:18 +0300

    add gitignore

--- VkLogin/Views/FriendCell.swift
+++ VkLogin/Views/FriendCell.swift
@@ -5,4 +5,16 @@
 //  Created by Konstantin on 02.07.2022.
 //
 
-import Foundation
+import UIKit
+
+class FriendCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: "")
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
